bd085979115d8b71954329674f6d9067e877dacf
Alexander Maryan <maryan.sasha@bk.ru>
2024-11-09 17:39:34 +0300

    fixes_after_review

--- src/main/java/ru/mai/lessons/rpks/impl/DatabaseDriver.java
+++ src/main/java/ru/mai/lessons/rpks/impl/DatabaseDriver.java
@@ -1,8 +1,5 @@
 package ru.mai.lessons.rpks.impl;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-
 import ru.mai.lessons.rpks.IDatabaseDriver;
 import ru.mai.lessons.rpks.exception.FieldNotFoundInTableException;
 import ru.mai.lessons.rpks.exception.WrongCommandFormatException;
@@ -11,23 +8,21 @@ import ru.mai.lessons.rpks.impl.DB.CsvFileData;
 import ru.mai.lessons.rpks.impl.DB.CsvFiles;
 import ru.mai.lessons.rpks.impl.Parsing.*;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.*;
 
 public class DatabaseDriver implements IDatabaseDriver {
   public static final String PATH_CACHE_FILE = "src/test/resources/cache_file.txt";
+  ConcurrentHashMap<String, List<String>> cacheMap = null;
   @Override
   public List<String> find(String studentsCsvFile, String groupsCsvFile, String subjectsCsvFile,
                            String gradeCsvFile, String command) throws WrongCommandFormatException, FieldNotFoundInTableException {
-    List<String> resultData = new ArrayList<>();
-    if (!checkCacheFile(command, resultData)) {
-      return null;
-    } else if (!resultData.isEmpty()) {
-      return resultData;
+    if (cacheMap == null) {
+      cacheMap = new ConcurrentHashMap<>();
+    } else if (cacheMap.containsKey(command)) {
+      return cacheMap.get(command);
     }
+    List<String> resultData = new ArrayList<>();
     CsvFileData csvData = new CsvFileData();
     csvData = ParseFiles.getCsvData(studentsCsvFile, groupsCsvFile, subjectsCsvFile, gradeCsvFile);
     if (csvData == null) {
@@ -46,49 +41,8 @@ public class DatabaseDriver implements IDatabaseDriver {
       throw new FieldNotFoundInTableException("Wrong field in query.");
     }
     if (resultData != null && !resultData.isEmpty()) {
-      pushInCacheFile(command, resultData);
+      cacheMap.put(command, resultData);
     }
     return resultData;
-  }
-  public boolean checkCacheFile(String command, List<String> resultData) {
-    File cacheFile = new File(PATH_CACHE_FILE);
-    try {
-      if (!cacheFile.exists()) {
-          cacheFile.createNewFile();
-      }
-    } catch (IOException e) {
-        e.printStackTrace();
-        return false;
-    }
-    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(cacheFile))) {
-      String line;
-      while ((line = bufferedReader.readLine()) != null) {
-        if (Objects.equals(line, command)) {
-          line = bufferedReader.readLine();
-          while (line != null && !line.trim().isEmpty()) {
-            resultData.add(line);
-            line = bufferedReader.readLine();
-          }
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      return false;
-    }
-    return true;
-  }
-  public void pushInCacheFile(String command, List<String> resultData) {
-    File cacheFile = new File(PATH_CACHE_FILE);
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(cacheFile, true))) {
-      writer.write(command);
-      writer.newLine();
-      for (String data : resultData) {
-        writer.write(data);
-        writer.newLine();
-      }
-      writer.newLine();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 }
