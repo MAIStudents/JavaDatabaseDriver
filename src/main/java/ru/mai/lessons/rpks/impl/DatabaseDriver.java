f15381e5a3d28c80b62bf80795005134fc4bb010
ukorp <ukorp63@gmail.com>
2024-11-24 14:32:28 +0300

    feat: add cache

--- src/main/java/ru/mai/lessons/rpks/impl/DatabaseDriver.java
+++ src/main/java/ru/mai/lessons/rpks/impl/DatabaseDriver.java
@@ -10,14 +10,20 @@ import ru.mai.lessons.rpks.impl.parsers.CommandParser;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.WeakHashMap;
 
 public class DatabaseDriver implements IDatabaseDriver {
 
     private static final List<String> fileList = List.of("grade.csv", "groups.csv", "students.csv", "subjects.csv");
 
+    private static final WeakHashMap<String, List<String>> queryMap = new WeakHashMap<>();
+
     @Override
     public List<String> find(String studentsCsvFile, String groupsCsvFile, String subjectsCsvFile,
                              String gradeCsvFile, String command) throws WrongCommandFormatException, FieldNotFoundInTableException {
+        if (queryMap.containsKey(command)) {
+            return queryMap.get(command);
+        }
         CommandParser parser = new CommandParser();
         List<String> select = new ArrayList<>();
         List<String> where = new ArrayList<>();
@@ -32,7 +38,9 @@ public class DatabaseDriver implements IDatabaseDriver {
                 .files(fileList)
                 .build();
         try {
-            return executing.execute();
+            List<String> resultList = executing.execute();
+            queryMap.put(command, resultList);
+            return resultList;
         } catch (IOException e) {
             throw new WrongCommandFormatException(e.getMessage());
         } catch (FieldNotFoundInTableException e) {
